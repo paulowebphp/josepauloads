e8cda60b3ec3d4be274b81a744728c4da2555a84
paulowebphp <paulowebphp@gmail.com>
2022-03-07 13:39:48 -0300

    Debug

---
+++ site-landing3_backup.php
@@ -0,0 +1,74 @@
+<?php 
+
+//use \Core\Page;
+use \Core\PageLanding;
+//use \Core\Validate;
+//use \Core\Mailer;
+use \Core\Rule;
+//use \Core\CodeFactory;
+use \Core\Model\Landing;
+use \Core\Model\Feature;
+
+
+
+
+
+
+
+
+
+
+$app->get( '/googlemeunegocio', function()
+{
+	
+
+	//$link1 = 'https://wa.me/5531984050125?text=Olá,%20preciso%20de%20um%20Gestor%20de%20Tráfego%20e%20gostaria%20de%20marcar%20uma%20Call!';
+	$link1 = 'https://bit.ly/gestorgooglemeunegocio';
+
+
+
+
+
+	$page = new PageLanding();
+
+	$page->setTpl(
+
+		"landing3".DIRECTORY_SEPARATOR."index",
+
+
+		[
+
+			'link1'=>$link1,
+			'error'=>Landing::getError()
+
+		]
+	
+	);//end setTpl
+
+});//END route
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
